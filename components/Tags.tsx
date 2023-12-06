e9ad0ffb6de5b37cda8fea36b19be621206d964c
Subset Softwares <subsetsoftwares@gmail.com>
2023-12-06 23:25:51 +0530

    added hyperlink to tags

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -1,22 +1,25 @@
 import { TagCount } from "@/types/tag";
+import Link from "next/link";
 import "./Tags.scss";
 
 type Props = { tags: TagCount[]; title: string; showCount?: boolean };
 
-const Tags = ({ tags, title, showCount }: Props) => {
+function getTagsLink(value: TagCount) {
+  return `/tag?page=1&limit=10&tag=${value.tag}`;
+}
+
+export default function Tags({ tags, title, showCount }: Props) {
   return (
     <div className="tags">
       <h2 className="section-title">{title}</h2>
       <div className="section">
         {tags.map((item) => (
-          <div className="tag" key={item.tag}>
+          <Link className="tag" key={item.tag} href={getTagsLink(item)}>
             <div className="value">{item.tag}</div>
             {showCount && <div className="count">{item.count}</div>}
-          </div>
+          </Link>
         ))}
       </div>
     </div>
   );
-};
-
-export default Tags;
+}
