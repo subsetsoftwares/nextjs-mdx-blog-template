db2bb0831a3637182bc3197369a086534e013cdb
Subset Softwares <subsetsoftwares@gmail.com>
2023-12-09 21:21:03 +0530

    updated trailingComma settings and styles

--- components/RenderArticle.tsx
+++ components/RenderArticle.tsx
@@ -6,7 +6,7 @@ import SyntaxHighlighter from "./SyntaxHighlighter";
 export default function RenderArticle({ article }: { article: Article }) {
   return (
     <article className={styles["rendered-article"]}>
-      <h1 className={"section-title"}>{article.meta.title}</h1>
+      <h1 className={styles["section-title"]}>{article.meta.title}</h1>
       <SyntaxHighlighter>
         <Markdown className={"markdown-preview"}>{article.content}</Markdown>
       </SyntaxHighlighter>
